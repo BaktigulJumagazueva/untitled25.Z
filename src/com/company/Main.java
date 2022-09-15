7f059d0beac41dba148dca222b98b08a4093151b
13 <baktyguljumagazyeva@gmail.com>
2022-09-15 09:51:13 +0600

    DZ25

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,17 @@
+package com.company;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        HashMap<Car, CarInformation> map = new HashMap<>();
+        map.put(new Car(43, 4376), new CarInformation(2020, "Mersedes", 25000, "black"));
+        map.put(new Car(87, 9854), new CarInformation(2021, "Honda", 50000, "white"));
+        for (Map.Entry entry : map.entrySet()) {
+            System.out.println(entry.getKey());
+            System.out.println(entry.getValue());
+        }
+
     }
 }
